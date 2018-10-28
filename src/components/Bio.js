a182ce2645c39cf7da62c8d66e9ed4f1acb89dd9
tkrplus <niwa.buffer@gmail.com>
2018-10-28 13:25:41 +0900

    🐟

---
+++ src/components/Bio.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+// Import typefaces
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+
+import { rhythm } from '../utils/typography'
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          marginBottom: rhythm(2.5),
+        }}
+      >
+        <p>
+          T++
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Bio
