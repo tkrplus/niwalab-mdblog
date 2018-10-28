09b0ab47e1c2bc0afecf6bcc7d0d90c07ddd5970
tkrplus <niwa.buffer@gmail.com>
2018-10-29 00:18:17 +0900

    Updates

--- .github/main.workflow
+++
@@ -1,8 +0,0 @@
-workflow "Test workflow" {
-  on = "push"
-  resolves = ["Dependencies"]
-}
-
-action "Dependencies" {
-  uses = "./actions/action-dependencies"
-}
---
+++ sw.js
@@ -0,0 +1,103 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("workbox-v3.5.0/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "workbox-v3.5.0"});
+
+workbox.core.setCacheNameDetails({prefix: "gatsby-plugin-offline"});
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "webpack-runtime-ace5941546b30d9de0e6.js"
+  },
+  {
+    "url": "app-7f15fe9671788781dc7e.js"
+  },
+  {
+    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-0a722e7e178d3c22dc4e.js"
+  },
+  {
+    "url": "index.html",
+    "revision": "3f2aab64ec7ec1a262596562083e309a"
+  },
+  {
+    "url": "offline-plugin-app-shell-fallback/index.html",
+    "revision": "58de9734a0d8934e48ff48aafa46f3bb"
+  },
+  {
+    "url": "0.7cf4e57993dd5221745e.css"
+  },
+  {
+    "url": "0-092177f2cbf775f19482.js"
+  },
+  {
+    "url": "component---src-pages-index-js-4d2dd4dbd5413886cfe0.js"
+  },
+  {
+    "url": "static/d/303/path---index-6a9-8jOkSZ4eRUn6AqL7BXLUJeEZsA.json",
+    "revision": "98abb15d0ed0894b430255afc3e75003"
+  },
+  {
+    "url": "component---src-pages-404-js-a53cd175da9a5d367574.js"
+  },
+  {
+    "url": "static/d/164/path---404-html-516-62a-NZuapzHg3X9TaN1iIixfv1W23E.json",
+    "revision": "c2508676a2f33ea9f1f0bf472997f9a0"
+  },
+  {
+    "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json",
+    "revision": "c2508676a2f33ea9f1f0bf472997f9a0"
+  },
+  {
+    "url": "manifest.webmanifest",
+    "revision": "3435901834b88a2def90ccbdf3c28302"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/niwalab-mdblog/offline-plugin-app-shell-fallback/index.html", {
+  whitelist: [/^[^?]*([^.?]{5}|\.html)(\?.*)?$/],
+  blacklist: [/\?(.+&)?no-cache=1$/],
+});
+
+workbox.routing.registerRoute(/\.(?:png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/, workbox.strategies.staleWhileRevalidate(), 'GET');
+workbox.routing.registerRoute(/^https:/, workbox.strategies.networkFirst(), 'GET');
+"use strict";
+
+/* global workbox */
+self.addEventListener("message", function (event) {
+  var api = event.data.api;
+
+  if (api === "gatsby-runtime-cache") {
+    var resources = event.data.resources;
+    var cacheName = workbox.core.cacheNames.runtime;
+    event.waitUntil(caches.open(cacheName).then(function (cache) {
+      return Promise.all(resources.map(function (resource) {
+        return cache.add(resource).catch(function (e) {
+          // ignore TypeErrors - these are usually due to
+          // external resources which don't allow CORS
+          if (!(e instanceof TypeError)) throw e;
+        });
+      }));
+    }));
+  }
+});
