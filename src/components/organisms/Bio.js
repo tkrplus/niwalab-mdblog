1b05665a87938421929f623292f5afb7da8c41f1
tkrplus <niwa.buffer@gmail.com>
2018-11-08 08:28:36 +0900

    edit bio

--- src/components/organisms/Bio.js
+++ src/components/organisms/Bio.js
@@ -1,15 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
 
 class Bio extends React.Component {
   render () {
     return (
-      <div >
+      <Wrapper >
         <p>
-          T++
+          Niwa Takeru <br />
+          都内在住SE/SIer  28歳<br />
+          ServerSideを中心に、Front, PublicCloud, Nativeとマルチスタックなエンジニアを目指しています。<br />
+          アジャイルな開発を好み、コラボレーションできるチームの作り方を日々模索しています。<br />
+          Java/Spring/JavaScript/React/AWS/docker/k8s/Swift
         </p>
-      </div>
+      </Wrapper>
     )
   }
 }
+
+const Wrapper = styled.div`
+  margin: 0 15px 30px;
+`
 
 export default Bio
